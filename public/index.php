c4a5816bdcd85bb291c5075414a5260ee37c292b
Ajeet Chauhan <ajit.chauhan96@gmail.com>
2023-08-31 13:41:19 +0530

    Initial commit

---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php
+
+use App\Framework\App;
+
+include_once './../vendor/autoload.php';
+
+$app = new App();
+
+$app->run();
---
+++ src/Framework/App.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Framework;
+
+class App
+{
+    public function run()
+    {
+        echo "App is running...";
+    }
+}
