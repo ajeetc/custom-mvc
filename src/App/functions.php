d0c35db879f0084eefd527b8d20395ae43604df8
Ajeet Chauhan <ajit.chauhan96@gmail.com>
2023-09-03 11:35:22 +0530

    Added symfony dumper and removed dd conflict fun

--- src/App/functions.php
+++ src/App/functions.php
@@ -2,23 +2,19 @@
 
 declare(strict_types=1);
 
-function dd(mixed $value): void
-{
-    echo "<pre>";
-    var_dump($value);
-    echo "</pre>";
-    die;
+if (!function_exists('pp')) {
+    function pp(mixed $value): void
+    {
+        echo "<pre>";
+        print_r($value);
+        echo "</pre>";
+        die;
+    }
 }
 
-function pp(mixed $value): void
-{
-    echo "<pre>";
-    print_r($value);
-    echo "</pre>";
-    die;
-}
-
-function e(mixed $value): mixed
-{
-    return htmlspecialchars((string) $value);
+if (!function_exists('e')) {
+    function e(mixed $value): mixed
+    {
+        return htmlspecialchars((string) $value);
+    }
 }
